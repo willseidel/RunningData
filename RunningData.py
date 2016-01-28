784cae132864714fad78ba5a9605eccb2dde48bd
Tyson Sacco <tsacco@12mac10.local>
2016-01-27 22:05:49 -0800

    Scrape names from track meet data
    figured out how to write the path correctly

--- RunningData.py
+++ RunningData.py
@@ -3,7 +3,17 @@ import requests
 
 page 				= requests.get('http://results.tfmeetpro.com/Athletic_Timing/TrackTown_USA_High_Performance_Meet_2/results_28.html')
 tree 				= html.fromstring(page.content)
-runners 			= tree.xpath('//td[@class="athlete"]/text()')
+places				= tree.xpath('//td[@class="place"]/text()')
+runners 			= tree.xpath('//tr/td[@class="athlete"]/a/text()')
 times 				= tree.xpath('//td[@class="time"]/text()')
+print 'Places:', places
+print 'Runners: ', runners
+print 'Times: ', times
+page 				= requests.get('http://results.tfmeetpro.com/Athletic_Timing/TrackTown_USA_High_Performance_Meet_2/results_11.html')
+tree 				= html.fromstring(page.content)
+places				= tree.xpath('//td[@class="place"]/text()')
+runners 			= tree.xpath('//tr/td[@class="athlete"]/a/text()')
+times 				= tree.xpath('//td[@class="time"]/text()')
+print 'Places:', places
 print 'Runners: ', runners
 print 'Times: ', times 
